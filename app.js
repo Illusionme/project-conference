48ad292b4e1b1c50ee026440c57a2bf8879adaef
krishnachirumamilla <murali.chirumamilla@muralichiru-ltm.internal.salesforce.com>
2018-05-16 15:35:51 -0700

    Added a Listener port

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express();
 
 var htmlController = require('./controllers/htmlController');
 
+var port = process.env.PORT || 3000;
+
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'pug');
 
 htmlController(app);
 
-app.listen(3000);
+app.listen(port);
